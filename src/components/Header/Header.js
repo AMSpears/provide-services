79d7697e77c66eef2dc1664319b9214c9aaba6da
Angie Spears <angie.spears809@gmail.com>
2018-09-08 11:26:27 -0400

    relocate files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "./Header.css"
+
+const Header = () =>{
+    return(
+        <ul>
+            <li><a href= "#">Features</a></li>
+            <li><a href= "#">Use Cases</a></li>
+            <li><a href= "#">News</a></li>
+            <li><a href= "#">Provide.network</a></li>
+        </ul>
+    )
+}
+
+export default Header
