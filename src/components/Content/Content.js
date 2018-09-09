bc0afc2cf13322375e9ed0947f1a5777ad20c9ed
Angie Spears <angie.spears809@gmail.com>
2018-09-09 17:32:49 -0400

    add feature section

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import Logo from '../Img/provide_logo_dark-blue.png'
+import FeatureItem from "../Img/noun_Network_27392.png"
 import './Content.css'
 
 const Content = () => {
     return (
         <div>
             <section className ="intro">
-            <img id = "intro-logo" src= {Logo} alt = "Provide Services"/>
-            <h1>Loren ipsum dolor sit amet consectetur</h1>
-            <p>Loren ipsum dolor sit amet, consectetur adipiscing elit sed do eiurmud tempor incididunt</p>
-            <a href= "#"><button id= "sign-up"> Sign Up</button></a>
+                <img id = "intro-logo" src= {Logo} alt = "Provide Services"/>
+                <h1 id= "intro-header">Loren ipsum dolor sit amet consectetur</h1>
+                <p id= "intro-details">Loren ipsum dolor sit amet, consectetur adipiscing elit sed do eiurmud tempor incididunt</p>
+                <a href= "#"><button id= "sign-up"> Sign Up</button></a>
             </section>
+            <section className ="feature-section">
+                <h1 id= "feature-header">Lorem ipsum dolor sit amet consectetur</h1>
+                <p id= "feature-details">Loren ipsum dolor sit amet, consectetur adipiscing elit sed do eiurmud tempor incididunt</p>
+              <div className= "feature-items-container">
+                <div>
+                    <img className= "feature-item-img" src ={FeatureItem}/>
+                    <h2 className= "feature-item-title">Feature</h2>
+                    <p className = "feature-item-details">Loren ipsum dolor sit amet, consectetur adipiscing elit sed do</p>
+                </div>
+                <div>
+                    <img className= "feature-item-img" src ={FeatureItem}/>
+                    <h2 className= "feature-item-title">Feature</h2>
+                    <p className = "feature-item-details">Loren ipsum dolor sit amet, consectetur adipiscing elit sed do</p>
+                </div>
+                <div>
+                    <img className= "feature-item-img" src ={FeatureItem}/>
+                    <h2 className = "feature-item-title">Feature</h2>
+                    <p className = "feature-item-details">Loren ipsum dolor sit amet, consectetur adipiscing elit sed do</p>
+                </div>
+              </div>
 
+            </section>
         </div>
     )
 }
