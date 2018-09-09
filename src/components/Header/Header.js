f618621bc7e171b778150af7e2b62328055d90b6
Angie Spears <angie.spears809@gmail.com>
2018-09-09 10:07:03 -0400

    modify header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,18 +2,15 @@ import React from 'react'
 import LogoHeader from '../Img/provide_logo_white-background.png'
 import "./Header.css"
 
-const Header = () =>{
-    return(
-    <nav className = "navbar">
-        <a id="logo" href="/"><img src={LogoHeader} width="133px" height="47px" alt="Provide" /></a>
-        <ul id ="main-nav">
-            <li><a href="#" className ="nav-links" id= "features">Features</a></li>
-            <li><a href="#" className= "nav-links" id= "use-cases">Use Cases</a></li>
-            <li><a href="#" className= "nav-links" id= "news">News</a></li>
-            <li><a href="#" className= "nav-links" id= "provide-network">Provide.network</a></li>
-        </ul>
-    </nav>
-      
+const Header = () => {
+    return (
+        <ul id ="main-nav">      
+            <a id="logo" href="/"><img src={LogoHeader} width="133px" height="47px" alt="Provide" /></a>
+            <li><a href="#" className ="nav-links">Features</a></li>
+            <li><a href="#" className= "nav-links">Use Cases</a></li>
+            <li><a href="#" className= "nav-links">News</a></li>
+            <li><a href="#" className= "nav-links">Provide.network</a></li>
+        </ul>  
     )
 }
 
